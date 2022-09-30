daf4a4c2dfd6b3947464f018d216ceca114ec602
brorae <jhr3239@gmail.com>
2022-09-30 10:07:01 +0900

    feat: DI 컨테이너 구현

--- di/src/test/java/nextstep/study/di/stage3/context/DIContainer.java
+++ di/src/test/java/nextstep/study/di/stage3/context/DIContainer.java
@@ -1,6 +1,11 @@
 package nextstep.study.di.stage3.context;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
 import java.util.Set;
+import java.util.stream.Collectors;
+import nextstep.study.ConsumerWrapper;
+import nextstep.study.FunctionWrapper;
 
 /**
  * 스프링의 BeanFactory, ApplicationContext에 해당되는 클래스
@@ -10,11 +15,32 @@ class DIContainer {
     private final Set<Object> beans;
 
     public DIContainer(final Set<Class<?>> classes) {
-        this.beans = Set.of();
+        this.beans = classes.stream()
+                .map(FunctionWrapper.apply(Class::getDeclaredConstructor))
+                .peek(constructor -> constructor.setAccessible(true))
+                .map(FunctionWrapper.apply(Constructor::newInstance))
+                .collect(Collectors.toUnmodifiableSet());
+        beans.forEach(this::setFields);
+    }
+
+    private void setFields(Object bean) {
+        Field[] declaredFields = bean.getClass().getDeclaredFields();
+        for (Field declaredField : declaredFields) {
+            Class<?> type = declaredField.getType();
+            declaredField.setAccessible(true);
+
+            beans.stream()
+                    .filter(type::isInstance)
+                    .forEach(ConsumerWrapper.accept(matchBean -> declaredField.set(bean, matchBean)));
+        }
     }
 
     @SuppressWarnings("unchecked")
     public <T> T getBean(final Class<T> aClass) {
-        return null;
+        return beans.stream()
+                .filter(aClass::isInstance)
+                .findFirst()
+                .map(bean -> (T) bean)
+                .orElseThrow(() -> new IllegalArgumentException("bean not found"));
     }
 }

defef9e5cf36ed17c5ba745f7e709a80239dd736
brorae <jhr3239@gmail.com>
2022-10-01 16:49:44 +0900

    feat: Inject 어노테이션 기반 DI 컨테이너 구현

--- di/src/test/java/nextstep/study/di/stage4/annotations/DIContainer.java
+++ di/src/test/java/nextstep/study/di/stage4/annotations/DIContainer.java
@@ -1,6 +1,13 @@
 package nextstep.study.di.stage4.annotations;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.util.Arrays;
 import java.util.Set;
+import java.util.stream.Collectors;
+import nextstep.study.ConsumerWrapper;
+import nextstep.study.FunctionWrapper;
+import org.reflections.Reflections;
 
 /**
  * 스프링의 BeanFactory, ApplicationContext에 해당되는 클래스
@@ -10,15 +17,47 @@ class DIContainer {
     private final Set<Object> beans;
 
     public DIContainer(final Set<Class<?>> classes) {
-        this.beans = Set.of();
+        this.beans = createBeans(classes);
+        beans.forEach(this::setFields);
     }
 
     public static DIContainer createContainerForPackage(final String rootPackageName) {
-        return null;
+        Set<Class<?>> allClassesInPackage = ClassPathScanner.getAllClassesInPackage(rootPackageName);
+        return allClassesInPackage.stream()
+                .filter(clazz -> clazz.isAnnotationPresent(Service.class) || clazz.isAnnotationPresent(Repository.class))
+                .collect(Collectors.collectingAndThen(Collectors.toUnmodifiableSet(), DIContainer::new));
     }
+
+    private Set<Object> createBeans(Set<Class<?>> classes) {
+        return classes.stream()
+                .map(FunctionWrapper.apply(Class::getDeclaredConstructor))
+                .peek(constructor -> constructor.setAccessible(true))
+                .map(FunctionWrapper.apply(Constructor::newInstance))
+                .collect(Collectors.toUnmodifiableSet());
+    }
+
+    private void setFields(Object bean) {
+        Arrays.stream(bean.getClass().getDeclaredFields())
+                .filter(field -> field.isAnnotationPresent(Inject.class))
+                .forEach(field -> setFields(bean, field));
+    }
+
+    private void setFields(Object bean, Field field) {
+        Class<?> type = field.getType();
+        field.setAccessible(true);
+
+        beans.stream()
+                .filter(type::isInstance)
+                .forEach(ConsumerWrapper.accept(matchBean -> field.set(bean, matchBean)));
+    }
+
 
     @SuppressWarnings("unchecked")
     public <T> T getBean(final Class<T> aClass) {
-        return null;
+        return beans.stream()
+                .filter(aClass::isInstance)
+                .findFirst()
+                .map(bean -> (T) bean)
+                .orElseThrow(() -> new IllegalArgumentException("bean not found"));
     }
 }
